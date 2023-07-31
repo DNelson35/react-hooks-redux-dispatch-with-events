93b1ee0863e7073b012a16530a94481c86c1f69a
Damien Nelson <dhnisaman@gmail.com>
2023-07-30 20:19:30 -0400

    commit tree 225b1340c78ed6b989ce2b87b49a16b6f01ddba9

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,41 @@
 // add code snippets from README
+let state;
+const container = document.getElementById('container')
+
+
+function reducer(state = { count: 0 }, action){
+    switch(action.type){
+        case 'counter/increment':
+            return { count: state.count + 1}
+        case 'counter/decrement':
+            return { count: state.count - 1}
+        default:
+            return state
+    }
+}
+
+function dispatch(action){
+    state = reducer(state, action)
+    render()
+}
+
+
+function render(){
+    container.textContent = state.count
+}
+
+dispatch({  type: '@@INIT'})
+
+const addButton = document.getElementById('button')
+
+const decrementButton = document.getElementById('new_button')
+
+
+addButton.addEventListener('click', () => {
+    dispatch({ type: 'counter/increment' })
+})
+
+decrementButton.addEventListener('click', () => {
+    dispatch({ type: 'counter/decrement' })
+})
+
